daa45bc1a366e0d2a414ffc2ca9157816a291dea
Jean Campelo <jean93campelo@gmail.com>
2022-07-18 14:56:37 -0300

    Correção className da Barra Superior

--- src/TopBar.js
+++ src/TopBar.js
@@ -6,32 +6,32 @@ const icons = [
 
 export default function TopBar() {
     return (
-        <div class="navbar">
-            <div class="container">
-                <div class="logo">
+        <div className ="navbar">
+            <div className ="container">
+                <div className ="logo">
                     <ion-icon name="logo-instagram"></ion-icon>
-                    <div class="separador"></div>
+                    <div className ="separador"></div>
                     <img src="assets/img/logo.png" />
                 </div>
 
-                <div class="logo-mobile">
+                <div className ="logo-mobile">
                     <ion-icon name="logo-instagram"></ion-icon>
                 </div>
 
-                <div class="instagram-mobile">
+                <div className ="instagram-mobile">
                     <img src="assets/img/logo.png" />
                 </div>
 
-                <div class="pesquisa">
+                <div className ="pesquisa">
                     <input type="text" placeholder="Pesquisar" />
                 </div>
 
-                <div class='icones'>
+                <div className ='icones'>
                 {icons.map((icon) => <ion-icon name={icon}></ion-icon>)}
                 </div>
                
 
-                <div class="icones-mobile">
+                <div className ="icones-mobile">
                     <ion-icon name="paper-plane-outline"></ion-icon>
                 </div>
             </div>
