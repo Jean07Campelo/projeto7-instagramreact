a2525a6c874a9cc2bd617a81914c43ada4393e63
Jean Campelo <jean93campelo@gmail.com>
2022-07-18 14:22:16 -0300

    Componentização Posts com map em array de objeto, estrutura de base contendo um post

--- src/PostsBox.js
+++ src/PostsBox.js
@@ -1,7 +1,54 @@
-export default function PostsBox () {
+export default function PostsBox() {
     return (
-        <div>
-            
+        <div className='posts'>
+            {posts.map((post) =>
+                <div class="post">
+                    <div class="topo">
+                        <div class="usuario">
+                            <img src={post.image_profile} />
+                            {post.userName}
+                        </div>
+                        <div class="acoes">
+                            <ion-icon name="ellipsis-horizontal"></ion-icon>
+                        </div>
+                    </div>
+
+                    <div class="conteudo">
+                        <img src={post.user_publication} />
+                    </div>
+
+                    <div class="fundo">
+                        <div class="acoes">
+                            <div>
+                                <ion-icon name="heart-outline"></ion-icon>
+                                <ion-icon name="chatbubble-outline"></ion-icon>
+                                <ion-icon name="paper-plane-outline"></ion-icon>
+                            </div>
+                            <div>
+                                <ion-icon name="bookmark-outline"></ion-icon>
+                            </div>
+                        </div>
+
+                        <div class="curtidas">
+                            <img src={post.image_profile_liked} />
+                            <div class="texto">
+                                Curtido por <strong>{post.profile_liked}</strong> e <strong>outras {post.total_liked} pessoas</strong>
+                            </div>
+                        </div>
+                    </div>
+                </div>)
+            }
         </div>
     )
 }
+
+const posts = [
+    {
+        image_profile: 'assets/img/meowed.svg',
+        userName: 'Meow',
+        user_publication: 'assets/img/gato-telefone.svg',
+        image_profile_liked: 'assets/img/respondeai.svg',
+        profile_liked: 'respondeai',
+        total_liked: 102.522
+    },
+]
