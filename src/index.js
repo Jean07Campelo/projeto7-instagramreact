302b080e934faea7d30ad9091ee8192ceffe7ff0
Jean Campelo <jean93campelo@gmail.com>
2022-07-18 15:07:07 -0300

    Implementação Componente para versão Mobile

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import ReactDOM from 'react-dom';
 
 import TopBar from './TopBar';
 import Container from './Container';
+import Mobile from './Mobile.js';
 
 function App() {
     return (
         <div>
             <TopBar />
             <Container />
+            <Mobile />
         </div>
 
     )
